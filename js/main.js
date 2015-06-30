14a9a0c2151ad36436a93c5d73cab0ae3bbb11fb
Saren Currie <saren@sarencurrie.com>
2015-06-30 16:16:09 +1200

    Added additional banner image.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-	$('#menu_button').on('click', function () {
-		$('#mobile_menu').toggle();
-	});
+  $('#menu_button').on('click', function () {
+    $('#mobile_menu').toggle();
+  });
 
-	var i = 0;
-	var TOTAL_BANNERS = 4;
-	setInterval(function () {
-		if (i >= TOTAL_BANNERS) {i = 0}
-		$('#banner_image').attr('src', '/img/banner/' + i + '.jpg');
-		i++;
-	}, 5000);
+  var i = 0;
+  var TOTAL_BANNERS = 5;
+  setInterval(function () {
+  if (i >= TOTAL_BANNERS) {
+	  i = 0;
+  }
+  $('#banner_image').attr('src', '/img/banner/' + i + '.jpg');
+  i++;
+  }, 5000);
 });
